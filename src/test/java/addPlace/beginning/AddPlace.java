eb0d4a0248e9a1a96d78456f07465906ed24d2bc
aydinSerbest <aydinserbest34@gmail.com>
2024-03-10 14:41:37 +0100

    updated

--- src/test/java/addPlace/beginning/AddPlace.java
+++ src/test/java/addPlace/beginning/AddPlace.java
@@ -2,6 +2,7 @@ package addPlace.beginning;
 
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.json.JSONObject;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -10,26 +11,31 @@ public class AddPlace {
     public static void main(String[] args) {
         RestAssured.baseURI="https://rahulshettyacademy.com";
         String response = given().queryParam("key", "qaclick123")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}\n")
+                .body("""
+                        {
+                          "location": {
+                            "lat": -38.383494,
+                            "lng": 33.427362
+                          },
+                          "accuracy": 50,
+                          "name": "Frontline house",
+                          "phone_number": "(+91) 983 893 3937",
+                          "address": "29, side layout, cohen 09",
+                          "types": [
+                            "shoe park",
+                            "shop"
+                          ],
+                          "website": "http://google.com",
+                          "language": "French-IN"
+                        }
+                                                
+                        """)
                 .when().post("/maps/api/place/add/json")
                 .then().statusCode(200).body("scope", equalTo("APP"))
                 .extract().response().asString();
-        System.out.println("all response as json = " + response);
+        JSONObject json = new JSONObject(response);
+        System.out.println(json.toString(4)); // This will print the formatted response
+        //System.out.println("all response as json = " + response);
 
         JsonPath js = new JsonPath(response);
         Object id = js.get("place_id");
