6e154a2094879627cb1f3715357e7fc2ce5b8729
aydinSerbest <aydinserbest34@gmail.com>
2024-03-10 14:24:07 +0100

    initial commit

---
+++ src/test/java/beginning/AddPlace.java
@@ -0,0 +1,39 @@
+package beginning;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class AddPlace {
+    public static void main(String[] args) {
+        RestAssured.baseURI="https://rahulshettyacademy.com";
+        String response = given().queryParam("key", "qaclick123")
+                .body("{\n" +
+                        "  \"location\": {\n" +
+                        "    \"lat\": -38.383494,\n" +
+                        "    \"lng\": 33.427362\n" +
+                        "  },\n" +
+                        "  \"accuracy\": 50,\n" +
+                        "  \"name\": \"Frontline house\",\n" +
+                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                        "  \"address\": \"29, side layout, cohen 09\",\n" +
+                        "  \"types\": [\n" +
+                        "    \"shoe park\",\n" +
+                        "    \"shop\"\n" +
+                        "  ],\n" +
+                        "  \"website\": \"http://google.com\",\n" +
+                        "  \"language\": \"French-IN\"\n" +
+                        "}\n")
+                .when().post("/maps/api/place/add/json")
+                .then().statusCode(200).body("scope", equalTo("APP"))
+                .extract().response().asString();
+        System.out.println("all response as json = " + response);
+
+        JsonPath js = new JsonPath(response);
+        Object id = js.get("place_id");
+        System.out.println("id = " + id);
+    }
+
+}
