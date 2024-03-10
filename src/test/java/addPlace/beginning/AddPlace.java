9db28b40a6321bcdb9457d12980ad901542015ad
aydinSerbest <aydinserbest34@gmail.com>
2024-03-10 15:03:07 +0100

    updated

--- src/test/java/addPlace/beginning/AddPlace.java
+++ src/test/java/addPlace/beginning/AddPlace.java
@@ -36,6 +36,18 @@ public class AddPlace {
                 .when().post("/maps/api/place/add/json")
                 .then().statusCode(200).body("scope", equalTo("APP"))
                 .extract().response().asString();
+
+        /*
+        f you want to format your JSON response in a pretty way,
+        you can use the org.json library to format your JSON response.
+        This code first writes the response in a proper format.
+        In order to use the JSONObject class, you need to add the dependency of the org.json library to your project.
+        The toString() method has a specific version for the JSONObject class,
+        allowing you to return your JSON data as a string in a reader-friendly, or "pretty-printed," format.
+        The optional parameter (4) you pass signifies indentation.
+        This parameter determines how much indentation will be added before each JSON item.
+        This makes it easier to read complex and long JSON data.
+         */
         JSONObject json = new JSONObject(response);
         System.out.println(json.toString(4)); // This will print the formatted response
         //System.out.println("all response as json = " + response);
